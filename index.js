286fc4915c6224f26893878dcd6bc6a0e91f5f31
nagykrisa <nagykrisa@gmail.com>
2018-11-07 11:42:49 +0100

    html

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var http = require('http');
-var dt = require('./dateModul');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
+  fs.readFile('example_html.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write(data);
     res.end();
+  });
 }).listen(8080);
 console.log('Pelda alkalmazas elindult!');
 console.log('localhost:8080 on elerheto');
