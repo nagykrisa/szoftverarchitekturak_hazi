1ddcff2b2c7b3cd6562591a58be7ee38910324d4
nagykrisa <nagykrisa@gmail.com>
2018-11-11 21:54:31 +0100

    delete

--- model/storage.js
+++
@@ -1,8 +0,0 @@
-exports.Storage = class Storage {
-    constructor( id,name,longitude,latitude){
-      this.id = id;
-      this.name = name;
-      this.longitude = longitude;
-      this.latitude = latitude;
-    }
-}
--- model/truck.js
+++
@@ -1,9 +0,0 @@
-exports.Truck = class Truck{
-  constructor( id,current_location,speed_max,volume_max,load_max){
-    this.id = id;
-    this.current_location = current_location;
-    this.speed_max = speed_max;
-    this.volume_max = volume_max;
-    this.load_max = load_max;
-  }
-}
