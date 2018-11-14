cfaa276cdbc91956c91d5e55900195abf9448d38
Kazrath <f.balint5.3@gmail.com>
2018-11-14 22:40:36 +0100

    mergeconflict

--- models/Truck_Model.js
+++ models/Truck_Model.js
@@ -6,22 +6,13 @@ var Truck_Model = model.extend({
         this.collection("Trucks").find(query || {}).toArray(callback);
     },
     remove_Truck: function(ID, callback) {
-<<<<<<< HEAD
         this.collection("Trucks").findAndModify({_id: ID}, [], {}, {remove: true}, callback);
-=======
-        this.collection("Trucks").findAndModify({__id: ID}, [], {}, {remove: true}, callback);
->>>>>>> 550ea408ad0b253a6b29dbb232f9bd473f11f939
     },
     insert_Truck: function(data, callback) {
         this.collection("Trucks").insertOne(data, {}, callback || function(){ });
     },
-<<<<<<< HEAD
     update_Truck: function(id, data, callback) {
         this.collection("Trucks").updateOne({_id: id}, { $set: data }, {}, callback || function(){ });	
-=======
-    update_Truck: function(data, callback) {
-        this.collection("Trucks").update({_id: data.ID}, data, {}, callback || function(){ });	
->>>>>>> 550ea408ad0b253a6b29dbb232f9bd473f11f939
     }
 });
 module.exports = Truck_Model;
